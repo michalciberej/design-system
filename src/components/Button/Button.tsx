4b80c9ba8bd8d20f8fbb561a1d700318d7cb5678
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-21 23:27:09 +0200

    Add  IconButton

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,24 @@
 import { forwardRef } from "react";
-import styles from "./Button.styles";
+import { iconStyles, mainStyles } from "./Button.styles";
 import type { ButtonProps } from "./Button.types";
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (props: ButtonProps) => {
-    const { children, variant, ref, ...rest } = props;
+  (props: ButtonProps, ref: React.ForwardedRef<HTMLButtonElement>) => {
+    const { variant, children, icon, iconBefore, iconAfter, ...rest } = props;
+
+    if (icon) {
+      return (
+        <button ref={ref} className={iconStyles({ variant })} {...rest}>
+          {icon}
+        </button>
+      );
+    }
 
     return (
-      <button ref={ref} className={styles({ ...variant })} {...rest}>
+      <button ref={ref} className={mainStyles({ variant })} {...rest}>
+        {iconBefore && iconBefore}
         {children}
+        {iconAfter && iconAfter}
       </button>
     );
   },
