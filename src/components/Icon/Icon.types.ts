d934bf15ddf3f3a74c02110fa7dff5692e4b56ed
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-25 18:43:55 +0200

    Refactor IconProps type definition

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -4,6 +4,12 @@ import { styles } from "./Icon.styles";
 export type IconVariantProps = VariantProps<typeof styles>;
 
 export interface IconProps extends React.SVGAttributes<SVGElement> {
-  variant?: Pick<IconVariantProps, "variant">;
-  size?: Pick<IconVariantProps, "size">;
+  variant?:
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "success"
+    | "warning"
+    | "error";
+  size?: "sm" | "sm" | "md" | "lg";
 }
