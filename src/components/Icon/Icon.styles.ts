848a15cbb516e848eeb39d2e59138c591349e19b
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-25 18:43:28 +0200

    Add variants styles

--- src/components/Icon/Icon.styles.ts
+++ src/components/Icon/Icon.styles.ts
@@ -1,16 +1,29 @@
 import { tv } from "tailwind-variants";
 
 export const styles = tv({
+  base: "fill-none transition-all disabled:stroke-inherit dark:disabled:stroke-inherit",
   variants: {
     variant: {
-      primary: "fill-none stroke-blue-500",
-      secondary: "fill none stroke-red-500",
-      tertiary: "",
-      success: "",
-      warning: "",
-      error: "",
+      default: "enabled:stroke-inherit dark:enabled:stroke-inherit",
+      primary:
+        "enabled:stroke-primary-text-light dark:enabled:stroke-primary-text-dark",
+      secondary:
+        "enabled:stroke-secondary-text-light dark:enabled:stroke-secondary-text-dark",
+      tertiary:
+        "enabled:stroke-tertiary-text-light dark:enabled:stroke-tertiary-text-dark",
+      success: "enabled:stroke-success dark:enabled:stroke-success-emphasis",
+      warning: "enabled:stroke-warning dark:enabled:stroke-warning-emphasis",
+      error: "enabled:stroke-error dark:enabled:stroke-error-emphasis",
     },
-    outline: "fill-none",
-    size: {},
+    size: {
+      xs: "",
+      sm: "",
+      lg: "",
+      xl: "",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "sm",
   },
 });
