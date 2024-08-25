218784763f5c75ed55f539e47a5fce2bc20b9ee1
Michal Ciberej <michal.ciberej@gmail.com>
2024-08-25 18:42:22 +0200

    Update styles

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,13 +1,16 @@
 import { tv } from "tailwind-variants";
 
-const base = "rounded-1 flex items-center justify-center gap-2 max-w-content";
+const base =
+  "rounded-1 flex items-center justify-center duration-300 gap-2 max-w-content transition-all font-main disabled:bg-input-disabled-light disabled:text-input-text-disabled-light disabled:dark:text-input-text-disabled-dark dark:disabled:bg-input-disabled-dark hover:enabled:shadow-lg-light dark:hover:enabled:shadow-lg-dark disabled:stroke-input-text-disabled-light dark:disabled:stroke-input-text-disabled-dark disabled:cursor-not-allowed ";
 
 export const mainStyles = tv({
-  base: `px-[15px] py-[10px] ${base}`,
+  base: `px-[15px] py-[10px] [&>svg]:aspect-square [&>svg]:w-4 ${base}`,
   variants: {
     variant: {
-      primary: "bg-primaryL dark:bg-primaryD",
-      secondary: "bg-secondaryL dark:bg-secondaryD",
+      primary:
+        "dark:text-primary-text-light bg-primary-light dark:bg-primary-dark dark:text-white",
+      secondary:
+        "text-secondary-text-light dark:text-secondary-text-dark bg-secondary-light dark:bg-secondary-dark",
     },
   },
   defaultVariants: {
@@ -16,14 +19,19 @@ export const mainStyles = tv({
 });
 
 export const iconStyles = tv({
-  base: `p-[10px] ${base}`,
+  base: `enabled:ring-border-light dark:enabled:ring-border-dark bg-transparent p-[10px] enabled:ring-1 enabled:ring-inset hover:enabled:ring-2 hover:enabled:ring-primary-light dark:hover:enabled:ring-primary-dark ${base}`,
   variants: {
     variant: {
-      primary: "bg-primaryL dark:bg-primaryD",
-      secondary: "bg-primaryL dark:bg-primaryD",
+      default:
+        "[&>svg]:enabled:stroke-border-light [&>svg]:dark:enabled:stroke-border-dark [&>svg]:enabled:hover:stroke-primary-light [&>svg]:dark:enabled:hover:stroke-primary-dark",
+    },
+    size: {
+      small: "h-8 w-8 [&>svg]:aspect-square [&>svg]:w-3.5",
+      large: "h-10 w-10 [&>svg]:aspect-square [&>svg]:w-5",
     },
   },
   defaultVariants: {
-    variant: "primary",
+    variant: "default",
+    size: "large",
   },
 });
